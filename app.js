a27ba346da1e63f899433dfe55f824eee1fde39d
hosseinmh <akhoundi22@gmail.com>
2016-09-22 00:25:35 +0330

    add a log messege for server

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.get('/',function(req,res){
     res.send('<h1>we are usnig node js and express :)');
 });
 
-var server = app.listen('2000');
+var server = app.listen('2000',function(){
+    console.log('server is runnig ');
+});
