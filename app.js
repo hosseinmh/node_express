5e8c9277e0532d2f221ce017bd31d82695f04c2b
hosseinmh <akhoundi22@gmail.com>
2016-09-22 00:15:51 +0330

    seccond section is strat :)

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-var http =require('http');
-
-
-var server = http.createServer(function(request,respond) {
-    respond.writeHead(200,{"Content-Type":"text/html"});
-    respond.write('<h1>welcome to learning node js ');
-    respond.end();
-    
-});
-server.listen('2000',function(){
-    console.log('server is runnig in port 2000');
-});
+var express = require('express');
