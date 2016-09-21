6146ca6f4ff4086f144e31f0197987889b0fdd93
hosseinmh <akhoundi22@gmail.com>
2016-09-21 23:50:54 +0330

    now create a server by using http function and add option to it

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 var http =require('http');
 
 
+var server = http.createServer(function(request,respond) {
+    respond.writeHead(200,{content-type:'text/html'});
+    respond.write('<h1>welcome to learning node js ');
+    respond.end();
+    
+});
