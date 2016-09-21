60a38bdf99c5ed924966f155b47685c8f106b31c
hosseinmh <akhoundi22@gmail.com>
2016-09-21 23:55:31 +0330

    some issue fixed such as Content-Type

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var http =require('http');
 
 
 var server = http.createServer(function(request,respond) {
-    respond.writeHead(200,{content-type:'text/html'});
+    respond.writeHead(200,{"Content-Type":"text/html"});
     respond.write('<h1>welcome to learning node js ');
     respond.end();
     
 });
 server.listen('2000',function(){
-    consolel.log('server is runnig in port 2000');
+    console.log('server is runnig in port 2000');
 });
