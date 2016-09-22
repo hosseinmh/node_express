80ff3ccd86c3219ce1c12058eadaaf9ac84acea3
hosseinmh <akhoundi22@gmail.com>
2016-09-22 11:33:55 +0330

    an another bug fixed

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ var express = require('express');
 
 var app= express();
 
+app.set('port',process.env.PORT||2000);
+
 app.get('/',function(req,res){
-    res.send('<h1>we are usnig node js and express :)');
+    res.send('<h1>we are usnig node js and express :)</h1>');
 });
 
-app.set('port',process.env.PORT||2000);
+
 var dateFile =require('./data/data.json');
 
 
-var server = app.listen(('app.get('port)',function(){
+var server = app.listen(app.get('port'),function(){
     console.log('server is runnig on ' + app.get('port'));
 });
