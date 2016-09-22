ba15a1cd8d2dcda2ed87b117587b2f2413a62910
hosseinmh <akhoundi22@gmail.com>
2016-09-22 11:24:54 +0330

    add a  dynamic port selection

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ app.get('/',function(req,res){
     res.send('<h1>we are usnig node js and express :)');
 });
 
+app.set('port',process.env.PORT||2000);
 var dateFile =require('./data/data.json');
+
 
 var server = app.listen('2000',function(){
     console.log('server is runnig ');
