0a506db212203a5cc8a5813a7f10f54290982ee0
hosseinmh <akhoundi22@gmail.com>
2016-09-22 11:29:35 +0330

    a little bug in () solved

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.set('port',process.env.PORT||2000);
 var dateFile =require('./data/data.json');
 
 
-var server = app.listen('app.get('port)',function(){
+var server = app.listen(('app.get('port)',function(){
     console.log('server is runnig on ' + app.get('port'));
 });
