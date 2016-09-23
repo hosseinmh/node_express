ce1d9e4c86a90c159d92b459a2f3b8a2280bfcb2
hosseinmh <akhoundi22@gmail.com>
2016-09-23 12:21:24 +0330

    we handle a response

--- client.js
+++ client.js
@@ -1,7 +1,14 @@
 var http =require('http');
 
-
+var option {
+    hostname:"localhost" ,
+    port:"8000"    ,
+    method:"GET"    ,
+    path:"/"    
+};
 
 var request = http.request(option,function(res){
-    
+    res.on("data", function(chunk){
+       console.log(chunk) ;
+    });
 });
