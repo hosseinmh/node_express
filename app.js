add5204bf91c6cf6f3c725ac36dfaabdc1766488
hosseinmh <akhoundi22@gmail.com>
2016-09-23 19:28:21 +0330

    now to add some try and catch state to chekc the page that have

--- app.js
+++ app.js
@@ -6,4 +6,17 @@ var url = require('url');
 http.createServer(function(){
     var uri = url.parse(req.url).pathname;
     var fileName = path.join(precess.cwd(),unescape(uri));
+    console.log('loading ' +uri);
+    var states;
+    
+    try{
+        states= fs.lstatSync(fileName)    ;
+    }
+    catch{
+        res.weiteHead(404 , {'Content-Type':'text/plain'}); 
+    res.write('the page not founf 404 ');
+    res.end();
+    return;
+    }
+    
 });
