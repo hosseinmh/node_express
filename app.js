b5c30371515dc01c1527f211624a29315c96d3c6
hosseinmh <akhoundi22@gmail.com>
2016-09-23 10:16:26 +0330

    we add async function to read file in first section we try in false mode to show how readfile handle error  the #issue->>> in code we didnt write the file that we should read from it

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ console.log(process.cwd());
 var buffer = fs.readFileSync(process.cwd()+"\\info.txt");
 
 console.log(buffer.toString());
+
+
+var async_buffer = fs.readFile(process.cwd(),function(error,buff){
+    
+    if(error){
+        console.log(error);
+        console.log("somthing is wrong ");
+    }
+    else{
+        console.log("the weork is done ");
+    }
+        
+});
