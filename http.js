ecf643df423fcaadfcb2cd400173482191e86542
hosseinmh <akhoundi22@gmail.com>
2016-09-23 12:16:33 +0330

    some issue is fixed

--- http.js
+++ http.js
@@ -1,7 +1,7 @@
 var http =require('http');
 
 var server = http.createServer(function(req,res){
-    res.writeHead('Content-Type':'text/plain')   ;
+    res.writeHead(200,{"Content-Type":'text/plain'});
     if(req.method =='GET'){
         res.end('you send a get method ');
     }
@@ -12,4 +12,4 @@ var server = http.createServer(function(req,res){
         res.end('you send other type ');
     }
 }).listen('8000');
-consol.log('server on port 8000');
+console.log('server on port 8000');
