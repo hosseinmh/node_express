84761c84702c0fd54352843a3fcaab071ffada52
hosseinmh <akhoundi22@gmail.com>
2016-09-23 12:05:10 +0330

    we handle type of respond method

--- http.js
+++ http.js
@@ -1,1 +1,15 @@
 var http =require('http');
+
+var server = http.createServer(function(req,res){
+    res.writeHead('Content-Type':'text/plain')   ;
+    if(req.method =='GET'){
+        res.end('you send a get method ');
+    }
+    else if(req.method =='POST'){
+        res.end('you send a post method');
+    }
+    else{
+        res.end('you send other type ');
+    }
+}).listen('8000');
+consol.log('server on port 8000');
